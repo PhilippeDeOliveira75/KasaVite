915ec7016c21cc60709b3b0ef01ce5076aa345d1
Philippe De Oliveira <philippe.deoliveira75@gmail.com>
2025-01-28 17:51:31 +0100

    maj burger menu v2

--- src/components/Modal/modal.jsx
+++ src/components/Modal/modal.jsx
@@ -1,25 +1,33 @@
 import './modal.scss'
 import { IoMdClose } from "react-icons/io"
+import { useState, useEffect } from 'react'
 
-const Modal = ({ isOpen, onClose, children }) => {
-    
-  if (!isOpen) return null
+function Modal ({ isOpen, onClose, children }) {
+  const [isClosing, setIsClosing] = useState(false)
+
+  useEffect(() => {
+    if (isOpen) {
+      setIsClosing(false)  // Reset the closing state when the modal is opened
+    }
+  }, [isOpen])
 
   const handleClose = () => {
-    onClose()
+    setIsClosing(true)
+    setTimeout(() => {
+      onClose()
+    }, 300) // Delay to match the duration of the closing animation
   }
 
-  return (
+  if (!isOpen && !isClosing) return null
 
-    <div className="modal-overlay" onClick={handleClose}>
+  return (
+    <div className={`modal-overlay ${isClosing ? 'close' : ''}`} onClick={handleClose}>
       <button className="modal-close" onClick={handleClose}>
         <IoMdClose />
       </button>
       {children}
     </div>
-
   )
-
 }
 
 export default Modal
